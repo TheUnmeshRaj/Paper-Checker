978be7baaeb5a6decbdb212bde689af346c5546e
TheUnmeshRaj <114986695+TheUnmeshRaj@users.noreply.github.com>
2024-12-09 10:50:01 +0530

    would try to later add, kivy to make an apk that works

--- utils.py
+++ utils.py
@@ -2,20 +2,21 @@ import cv2
 import numpy as np
 
 
-def stackImage(imgArray,scale,labels=[]):
+def stackImage(imgArray, scale, labels=[]):
     rows = len(imgArray)
     cols = len(imgArray[0])
     rowsAvailable = isinstance(imgArray[0], list)
     width = imgArray[0][0].shape[1]
     height = imgArray[0][0].shape[0]
     if rowsAvailable:
-        for x in range ( 0, rows):
+        for x in range(0, rows):
             for y in range(0, cols):
                 imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
-                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+                if len(imgArray[x][y].shape) == 2:
+                    imgArray[x][y] = cv2.cvtColor(imgArray[x][y], cv2.COLOR_GRAY2BGR)
         imageBlank = np.zeros((height, width, 3), np.uint8)
-        hor = [imageBlank]*rows
-        hor_con = [imageBlank]*rows
+        hor = [imageBlank] * rows
+        hor_con = [imageBlank] * rows
         for x in range(0, rows):
             hor[x] = np.hstack(imgArray[x])
             hor_con[x] = np.concatenate(imgArray[x])
@@ -24,98 +25,103 @@ def stackImage(imgArray,scale,labels=[]):
     else:
         for x in range(0, rows):
             imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
-            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
-        hor= np.hstack(imgArray)
-        hor_con= np.concatenate(imgArray)
+            if len(imgArray[x].shape) == 2:
+                imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor = np.hstack(imgArray)
+        hor_con = np.concatenate(imgArray)
         ver = hor
     if len(labels) != 0:
-        eachImgWidth= int(ver.shape[1] / cols)
+        eachImgWidth = int(ver.shape[1] / cols)
         eachImgHeight = int(ver.shape[0] / rows)
-        #print(eachImgHeight)
         for d in range(0, rows):
-            for c in range (0,cols):
-                cv2.rectangle(ver,(c*eachImgWidth,eachImgHeight*d),(c*eachImgWidth+len(labels[d][c])*13+27,30+eachImgHeight*d),(255,255,255),cv2.FILLED)
-                cv2.putText(ver,labels[d][c],(eachImgWidth*c+10,eachImgHeight*d+20),cv2.FONT_HERSHEY_COMPLEX,0.7,(255,0,255),2)
+            for c in range(0, cols):
+                cv2.rectangle(ver, (c * eachImgWidth, eachImgHeight * d),
+                              (c * eachImgWidth + len(labels[d][c]) * 13 + 27, 30 + eachImgHeight * d), (255, 255, 255),
+                              cv2.FILLED)
+                cv2.putText(ver, labels[d][c], (eachImgWidth * c + 10, eachImgHeight * d + 20), cv2.FONT_HERSHEY_COMPLEX,
+                            0.7, (255, 0, 255), 2)
     return ver
-  
-def rectContours(contours):
-  rect = []
-  maxArea=0
-  for i in contours:
-    area=cv2.contourArea(i)
-    if area>50:
-      peri = cv2.arcLength(i,True)
-      approx = cv2.approxPolyDP(i,0.02*peri,True)
-      if(len(approx) == 4):
-        rect.append(approx)
-  rect=sorted(rect,key=cv2.contourArea,reverse=True)
-  
-  return rect
-  
-def reorder(myPoints):
-    myPoints = myPoints.reshape((4, 2) ) 
-    print(myPoints)
-    myPointsNew = np.zeros((4, 1, 2), np.int32) 
-    add = myPoints.sum(1)
-    myPointsNew[0] = myPoints[np.argmin(add)]  
-    myPointsNew[3] =myPoints[np.argmax(add)]   
-    diff = np.diff(myPoints, axis=1)
-    myPointsNew[1] =myPoints[np.argmin(diff)]  
-    myPointsNew[2] = myPoints[np.argmax(diff)] 
 
+
+def rectContours(contours):
+    rect = []
+    maxArea = 0
+    for i in contours:
+        area = cv2.contourArea(i)
+        if area > 50:
+            peri = cv2.arcLength(i, True)
+            approx = cv2.approxPolyDP(i, 0.02 * peri, True)
+            if len(approx) == 4:
+                rect.append(approx)
+    rect = sorted(rect, key=cv2.contourArea, reverse=True)
+    return rect
+
+
+def reorder(myPoints):
+    myPoints = myPoints.reshape((4, 2))
+    myPointsNew = np.zeros((4, 1, 2), np.int32)
+    add = myPoints.sum(1)
+    myPointsNew[0] = myPoints[np.argmin(add)]
+    myPointsNew[3] = myPoints[np.argmax(add)]
+    diff = np.diff(myPoints, axis=1)
+    myPointsNew[1] = myPoints[np.argmin(diff)]
+    myPointsNew[2] = myPoints[np.argmax(diff)]
     return myPointsNew
 
-def splitBoxes(img):
-    rows = np.vsplit(img,5)
-    boxes=[]
-    for r in rows:
-        cols= np.hsplit(r,10)
-        for box in cols:
-            boxes.append(box)
-    return boxes    
 
-
-def splitBoxes(img):
-    rows = np.vsplit(img,5)
-    boxes=[]
+def splitBoxes(img, questions=5, choices=5):
+    rows = np.vsplit(img, questions)
+    boxes = []
     for r in rows:
-        cols= np.hsplit(r,5)
+        cols = np.hsplit(r, choices)
         for box in cols:
             boxes.append(box)
     return boxes
 
-def drawGrid(img,questions=5,choices=5):
-    secW = int(img.shape[1]/questions)
-    secH = int(img.shape[0]/choices)
-    for i in range (0,9):
-        pt1 = (0,secH*i)
-        pt2 = (img.shape[1],secH*i)
-        pt3 = (secW * i, 0)
-        pt4 = (secW*i,img.shape[0])
-        cv2.line(img, pt1, pt2, (255, 255, 0),2)
-        cv2.line(img, pt3, pt4, (255, 255, 0),2)
 
+def drawGrid(img, questions=5, choices=5):
+    secW = int(img.shape[1] / questions)
+    secH = int(img.shape[0] / choices)
+    for i in range(0, questions + 1):
+        pt1 = (0, secH * i)
+        pt2 = (img.shape[1], secH * i)
+        pt3 = (secW * i, 0)
+        pt4 = (secW * i, img.shape[0])
+        cv2.line(img, pt1, pt2, (255, 255, 0), 2)
+        cv2.line(img, pt3, pt4, (255, 255, 0), 2)
     return img
 
-def showAnswers(img,myIndex,grading,ans,questions=5,choices=5):
-     secW = int(img.shape[1]/questions)
-     secH = int(img.shape[0]/choices)
 
-     for x in range(0,questions):
-         myAns= myIndex[x]
-         cX = (myAns * secW) + secW // 2
-         cY = (x * secH) + secH // 2
-         if grading[x]==1:
-            myColor = (0,255,0)
-            #cv2.rectangle(img,(myAns*secW,x*secH),((myAns*secW)+secW,(x*secH)+secH),myColor,cv2.FILLED)
-            cv2.circle(img,(cX,cY),50,myColor,cv2.FILLED)
-         else:
-            myColor = (0,0,255)
-            #cv2.rectangle(img, (myAns * secW, x * secH), ((myAns * secW) + secW, (x * secH) + secH), myColor, cv2.FILLED)
+def showAnswers(img, myIndex, grading, ans, questions=5, choices=5):
+    secW = int(img.shape[1] / questions)
+    secH = int(img.shape[0] / choices)
+
+    for x in range(0, questions):
+        myAns = myIndex[x]
+        cX = (myAns * secW) + secW // 2
+        cY = (x * secH) + secH // 2
+        if grading[x] == 1:
+            myColor = (0, 255, 0)
+            cv2.circle(img, (cX, cY), 50, myColor, cv2.FILLED)
+        else:
+            myColor = (0, 0, 255)
             cv2.circle(img, (cX, cY), 50, myColor, cv2.FILLED)
 
             # CORRECT ANSWER
             myColor = (0, 255, 0)
             correctAns = ans[x]
-            cv2.circle(img,((correctAns * secW)+secW//2, (x * secH)+secH//2),
-            20,myColor,cv2.FILLED)
+            
+            if isinstance(correctAns, str):  # Handle non-numeric answers
+                if correctAns == 'A':
+                    correctAns = 0
+                elif correctAns == 'B':
+                    correctAns = 1
+                elif correctAns == 'C':
+                    correctAns = 2
+                elif correctAns == 'D':
+                    correctAns = 3
+                else:
+                    correctAns = -1  # For unexpected cases like 'Bonus'
+            
+            cv2.circle(img, ((correctAns * secW) + secW // 2, (x * secH) + secH // 2), radius=20, color=(0, 255, 0), thickness=5)
+
